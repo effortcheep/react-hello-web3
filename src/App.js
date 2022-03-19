d9b27d5853e26911b1d5f3ccde5a1b36b10db5f4
effortcheep <effortcheep@163.com>
2022-03-19 14:36:30 +0800

    chore: change hi function

--- src/App.js
+++ src/App.js
@@ -46,9 +46,10 @@ function App() {
           params: [
             {
               from: account,
-              to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
-              value: '0x29a2241af62c0000',
-              gasPrice: '0x09184e72a000',
+              to: '0x2c7cdb3712ba96327250c61f46c9332857e0fe59',
+              value: '0x11c37937e08000',
+              // gasPrice: '0x4c4b400',
+              gasPrice: '0x12a05f200',
               gas: '0x2710',
             },
           ],
